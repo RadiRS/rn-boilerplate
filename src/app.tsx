c34fcb03bb70c6764300fc241bc4087448f2c9e3
Radi Rusadi <radirusadi@192.168.1.3>
2022-08-24 17:10:20 +0700

    Installed & setup husky

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React, {FC} from 'react';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
