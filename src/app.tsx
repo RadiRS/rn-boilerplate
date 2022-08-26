f958de73dfbc1de270539f8c09c58253029cbaa1
Radi <radi.rusadi.s@gmail.com>
2022-08-26 17:18:34 +0700

    Installed, setup & tested react-native storybook

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
 
-import { RootNavigator } from '@/navigators';
-import { persistor, store } from './store';
-import './config/translations';
+// import { RootNavigator } from '@/navigators';
+// import { persistor, store } from './store';
+// import './config/translations';
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <RootNavigator />
-      </PersistGate>
-    </Provider>
-  );
-};
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//       <PersistGate persistor={persistor}>
+//         <RootNavigator />
+//       </PersistGate>
+//     </Provider>
+//   );
+// };
 
-export default App;
+// export default App;
+
+// Render Storybook
+export { default } from '../storybook';
