4697ac9dc041451a3249c283961963dec124bfe0
Radi <radi.rusadi.s@gmail.com>
2022-08-31 10:56:16 +0700

    Added test button component

---
+++ src/components/ui/button/button.test.tsx
@@ -0,0 +1,42 @@
+import 'react-native';
+import React from 'react';
+
+import { Text, Button } from '@/components/ui';
+import { renderWithRedux } from '@/helpers';
+import { fireEvent } from '@testing-library/react-native';
+
+describe('<Button />', () => {
+  it('should render component correcly', () => {
+    const rendered = renderWithRedux(<Button />).toJSON();
+
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it('should render text passed to children', () => {
+    const rendered = renderWithRedux(<Button>Hello World</Button>);
+
+    expect(rendered.queryByText('Hello World')).toBeTruthy();
+  });
+
+  it('should render component passed to children', () => {
+    const rendered = renderWithRedux(
+      <Button>
+        <Text>I love Indonesia</Text>
+      </Button>,
+    );
+
+    expect(rendered.queryByText('I love Indonesia')).toBeTruthy();
+  });
+
+  it('should call onPress', () => {
+    const onPress = jest.fn();
+
+    const rendered = renderWithRedux(
+      <Button testID="button" onPress={onPress} />,
+    );
+
+    fireEvent.press(rendered.getByTestId('button'));
+
+    expect(onPress).toBeCalled();
+  });
+});
