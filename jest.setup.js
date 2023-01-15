34bb9a67bce826c6c736fd540aab817950d2b122
Radi <radi.rusadi.s@gmail.com>
2023-01-15 15:08:50 +0700

    Upgraded react-native-version - Removed react-native-reanimated because have issue with new react-native version - Update android and ios setting for new react-native version

--- jest.setup.js
+++ jest.setup.js
@@ -6,15 +6,15 @@ import { renderWithRedux, shallowRenderWithRedux } from '@/helpers';
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
-jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
+// jest.mock('react-native-reanimated', () => {
+//   const Reanimated = require('react-native-reanimated/mock');
 
-  // The mock for `call` immediately calls the callback which is incorrect
-  // So we override it with a no-op
-  Reanimated.default.call = () => {};
+//   // The mock for `call` immediately calls the callback which is incorrect
+//   // So we override it with a no-op
+//   Reanimated.default.call = () => {};
 
-  return Reanimated;
-});
+//   return Reanimated;
+// });
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
