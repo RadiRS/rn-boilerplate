272d5f60c055a87994d52abde5c1d318c7705231
Radi <radi.rusadi.s@gmail.com>
2023-03-18 11:46:58 +0700

    Core: Upgraded react-native-version from 0.71.0 to 0.71.4

--- ios/RNB/AppDelegate.mm
+++ ios/RNB/AppDelegate.mm
@@ -11,6 +11,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"RNB";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
 
   [RNBootSplash initWithStoryboard:@"BootSplash" rootView:self.window.rootViewController.view];
 
